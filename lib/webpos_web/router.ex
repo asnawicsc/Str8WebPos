6d491220d7142cdc20caf1103997cc5f5c41bd54
Damien Lee <yithanglee@gmail.com>
2019-01-30 09:15:00 +0800

    error view router changes authorizations

--- lib/webpos_web/router.ex
+++ lib/webpos_web/router.ex
@@ -2,21 +2,33 @@ defmodule WebposWeb.Router do
   use WebposWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+    plug(Webpos.Authorization)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
-  scope "/", WebposWeb do
-    pipe_through :browser # Use the default browser stack
+  scope "/api", WebposWeb do
+    pipe_through(:api)
 
-    get "/", PageController, :index
+    get("/sales", ApiController, :webhook_get)
+    post("/sales", ApiController, :webhook_post)
+
+    post("/operations", ApiController, :webhook_post_operations)
+  end
+
+  scope "/:organization", WebposWeb do
+    # Use the default browser stack
+    pipe_through(:browser)
+
+    get("/", PageController, :index)
+    get("/*path", PageController, :no_page_found)
   end
 
   # Other scopes may use custom stacks.
