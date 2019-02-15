bbccfb84db73e4ef4857e8646dd8615fbe16d3e7
Your Nawi <asnawicsc@gmail.com>
2019-02-15 18:02:58 +0800

    update report

--- lib/webpos_web/controllers/page_controller.ex
+++ lib/webpos_web/controllers/page_controller.ex
@@ -1,5 +1,6 @@
 defmodule WebposWeb.PageController do
   use WebposWeb, :controller
+  require IEx
 
   def index(conn, _params) do
     render(conn, "index.html")
@@ -95,5 +96,110 @@ defmodule WebposWeb.PageController do
     staff_list = %{staffs: staff_data} |> Poison.encode!()
 
     send_resp(conn, 200, staff_list)
+  end
+
+  def sales_by_category(conn, params) do
+    conn
+    |> put_resp_content_type("text/csv")
+    |> put_resp_header(
+      "content-disposition",
+      "attachment; filename=\"Sales By Category.csv.csv\""
+    )
+    |> send_resp(200, csv_content(conn, params))
+  end
+
+  def csv_content(conn, params) do
+    branch = Repo.get_by(Restaurant, code: params["branch"])
+
+    all =
+      Repo.all(
+        from(
+          sd in Webpos.Reports.SalesDetail,
+          left_join: s in Webpos.Reports.Sale,
+          on: s.salesid == sd.salesid,
+          where:
+            s.salesdate >= ^params["start_date"] and s.salesdate <= ^params["end_date"] and
+              s.rest_name == ^branch.name,
+          group_by: [s.salesdate, sd.itemname, s.rest_name],
+          select: %{
+            salesdate: s.salesdate,
+            branch: s.rest_name,
+            itemname: sd.itemname,
+            sub_total: sum(sd.sub_total),
+            qty: sum(sd.qty)
+          }
+        )
+      )
+
+    csv_content = [
+      'Date',
+      'Branch',
+      'Item Name',
+      'Total Quantity',
+      'Sub Total'
+    ]
+
+    data =
+      for item <- all do
+        [item.salesdate, item.branch, item.itemname, item.qty, item.sub_total]
+      end
+
+    csv_content =
+      List.insert_at(data, 0, csv_content)
+      |> CSV.encode()
+      |> Enum.to_list()
+      |> to_string
+  end
+
+  def top_10_sales(conn, params) do
+    conn
+    |> put_resp_content_type("text/csv")
+    |> put_resp_header(
+      "content-disposition",
+      "attachment; filename=\"Top 10 Sales.csv.csv\""
+    )
+    |> send_resp(200, csv_content_top_10_sales(conn, params))
+  end
+
+  def csv_content_top_10_sales(conn, params) do
+    branch = Repo.get_by(Restaurant, code: params["branch"])
+
+    all =
+      Repo.all(
+        from(
+          sd in Webpos.Reports.SalesDetail,
+          left_join: s in Webpos.Reports.Sale,
+          on: s.salesid == sd.salesid,
+          where:
+            s.salesdate >= ^params["start_date"] and s.salesdate <= ^params["end_date"] and
+              s.rest_name == ^branch.name,
+          group_by: [sd.itemname],
+          select: %{
+            itemname: sd.itemname,
+            sub_total: sum(sd.sub_total),
+            qty: sum(sd.qty)
+          }
+        )
+      )
+      |> Enum.sort_by(fn x -> x.sub_total end)
+      |> Enum.reverse()
+      |> Enum.take(10)
+
+    csv_content = [
+      'Item Name',
+      'Total Quantity',
+      'Sub Total'
+    ]
+
+    data =
+      for item <- all do
+        [item.itemname, item.qty, item.sub_total]
+      end
+
+    csv_content =
+      List.insert_at(data, 0, csv_content)
+      |> CSV.encode()
+      |> Enum.to_list()
+      |> to_string
   end
 end
