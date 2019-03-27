99c14949dbd6246bc332cb0c763fc55f25d44203
Your Nawi <asnawicsc@gmail.com>
2019-03-27 14:24:06 +0800

    update

--- lib/webpos_web/channels/organization_channel.ex
+++ lib/webpos_web/channels/organization_channel.ex
@@ -26,7 +26,7 @@ defmodule WebposWeb.OrganizationChannel do
   defp authorized?(payload, code) do
     IO.inspect(payload)
     organization = Repo.get_by(Organization, code: code)
-    IO.inspect(payload["license_key"])
+
     password = Comeonin.Bcrypt.hashpwsalt(payload["license_key"])
     IO.inspect(organization)
 
@@ -93,17 +93,34 @@ defmodule WebposWeb.OrganizationChannel do
 
     list_year = list |> Enum.map(fn x -> x.year end) |> Enum.uniq()
 
+    organization = Repo.get_by(Organization, code: payload["organization_code"])
+
     sales =
-      Repo.all(
-        from(
-          i in Reports.Sale,
-          where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
-          select: %{
-            salesdate: i.salesdate,
-            sales: i.grand_total
-          }
+      if(payload["branch_name"] == "All Branch") do
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
         )
-      )
+      else
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where:
+              i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+                i.organization_id == ^organization.id and i.rest_name == ^payload["branch_name"],
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
+        )
+      end
 
     final =
       for item <- list_year do
@@ -132,31 +149,40 @@ defmodule WebposWeb.OrganizationChannel do
   end
 
   def handle_in("monthly_sales", payload, socket) do
-    # broadcast(socket, "shout", payload)
-    IO.inspect(payload["date_start"])
-    IO.inspect(payload["date_end"])
-
-    IO.inspect(payload)
-    IO.inspect(socket)
-
     date_start = Date.from_iso8601!(payload["date_start"])
     date_end = Date.from_iso8601!(payload["date_end"])
 
+    organization = Repo.get_by(Organization, code: payload["organization_code"])
     list = Date.range(date_start, date_end)
 
     list_month = list |> Enum.map(fn x -> x.month end) |> Enum.uniq()
 
     sales =
-      Repo.all(
-        from(
-          i in Reports.Sale,
-          where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
-          select: %{
-            salesdate: i.salesdate,
-            sales: i.grand_total
-          }
+      if(payload["branch_name"] == "All Branch") do
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
         )
-      )
+      else
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where:
+              i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+                i.organization_id == ^organization.id and i.rest_name == ^payload["branch_name"],
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
+        )
+      end
 
     final =
       for item <- list_month do
@@ -178,38 +204,45 @@ defmodule WebposWeb.OrganizationChannel do
         total
       end
 
-    IO.inspect(final)
-
     broadcast(socket, "monthly_sales_reply", %{result: final})
     {:noreply, socket}
   end
 
   def handle_in("daily_sales", payload, socket) do
-    # broadcast(socket, "shout", payload)
-    IO.inspect(payload["date_start"])
-    IO.inspect(payload["date_end"])
-
-    IO.inspect(payload)
-    IO.inspect(socket)
-
     date_start = Date.from_iso8601!(payload["date_start"])
     date_end = Date.from_iso8601!(payload["date_end"])
 
     list = Date.range(date_start, date_end)
 
     list_day = list |> Enum.map(fn x -> x.day end) |> Enum.uniq()
+    organization = Repo.get_by(Organization, code: payload["organization_code"])
 
     sales =
-      Repo.all(
-        from(
-          i in Reports.Sale,
-          where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
-          select: %{
-            salesdate: i.salesdate,
-            sales: i.grand_total
-          }
+      if(payload["branch_name"] == "All Branch") do
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
         )
-      )
+      else
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where:
+              i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+                i.organization_id == ^organization.id and i.rest_name == ^payload["branch_name"],
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
+        )
+      end
 
     final =
       for item <- list_day do
@@ -231,53 +264,112 @@ defmodule WebposWeb.OrganizationChannel do
         total
       end
 
-    IO.inspect(final)
+    result2 =
+      if(payload["branch_name"] == "All Branch") do
+        Repo.all(
+          from(i in Reports.Sale,
+            left_join: p in Reports.SalesDetail,
+            on: i.salesid == p.salesid,
+            where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
+            group_by: [p.itemname],
+            order_by: [desc: sum(p.sub_total)],
+            select: %{
+              item: p.itemname,
+              sales: sum(p.sub_total)
+            },
+            limit: 10
+          )
+        )
+        |> Enum.filter(fn x -> x.item != nil end)
+      else
+        Repo.all(
+          from(i in Reports.Sale,
+            left_join: p in Reports.SalesDetail,
+            on: i.salesid == p.salesid,
+            where:
+              i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+                i.organization_id == ^organization.id and i.rest_name == ^payload["branch_name"],
+            group_by: [p.itemname],
+            order_by: [desc: sum(p.sub_total)],
+            select: %{
+              item: p.itemname,
+              sales: sum(p.sub_total)
+            },
+            limit: 10
+          )
+        )
+        |> Enum.filter(fn x -> x.item != nil end)
+      end
 
-    broadcast(socket, "daily_sales_reply", %{result: final})
+    result3 =
+      for item <- result2 do
+        sales = item.sales
+
+        new_sales =
+          if sales != 0.0 do
+            sales |> Decimal.to_string()
+          else
+            "0.0"
+          end
+
+        %{
+          item: item.item,
+          sales: new_sales
+        }
+      end
+
+    broadcast(socket, "daily_sales_reply", %{result: final, result2: result3})
     {:noreply, socket}
   end
 
   def handle_in("top_10_item", payload, socket) do
-    # broadcast(socket, "shout", payload)
-    IO.inspect(payload["date_start"])
-    IO.inspect(payload["date_end"])
-
-    IO.inspect(payload)
-    IO.inspect(socket)
-
     date_start = Date.from_iso8601!(payload["date_start"])
     date_end = Date.from_iso8601!(payload["date_end"])
+    organization = Repo.get_by(Organization, code: payload["organization_code"])
 
-    final =
-      Repo.all(
-        from(i in Reports.Sale,
-          left_join: p in Reports.SalesDetail,
-          on: i.salesid == p.salesid,
-          where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
-          group_by: [p.itemname],
-          order_by: [desc: sum(p.sub_total)],
-          select: %{
-            item: p.itemname,
-            sales: sum(p.sub_total)
-          },
-          limit: 10
+    sales =
+      if(payload["branch_name"] == "All Branch") do
+        Repo.all(
+          from(i in Reports.Sale,
+            left_join: p in Reports.SalesDetail,
+            on: i.salesid == p.salesid,
+            where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
+            group_by: [p.itemname],
+            order_by: [desc: sum(p.sub_total)],
+            select: %{
+              item: p.itemname,
+              sales: sum(p.sub_total)
+            },
+            limit: 10
+          )
         )
-      )
-      |> Enum.filter(fn x -> x.item != nil end)
+        |> Enum.filter(fn x -> x.item != nil end)
+      else
+        Repo.all(
+          from(i in Reports.Sale,
+            left_join: p in Reports.SalesDetail,
+            on: i.salesid == p.salesid,
+            where:
+              i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+                i.organization_id == ^organization.id and i.rest_name == ^payload["branch_name"],
+            group_by: [p.itemname],
+            order_by: [desc: sum(p.sub_total)],
+            select: %{
+              item: p.itemname,
+              sales: sum(p.sub_total)
+            },
+            limit: 10
+          )
+        )
+        |> Enum.filter(fn x -> x.item != nil end)
+      end
 
-    IO.inspect(final)
-
-    broadcast(socket, "top_10_item_reply", %{result: final})
+    broadcast(socket, "top_10_item_reply", %{result: sales})
     {:noreply, socket}
   end
 
   def handle_in("today_sales", payload, socket) do
-    # broadcast(socket, "shout", payload)
-    IO.inspect(payload["date_start"])
-    IO.inspect(payload["date_end"])
-
-    IO.inspect(payload)
-    IO.inspect(socket)
+    organization = Repo.get_by(Organization, code: payload["organization_code"])
 
     date_start = Date.from_iso8601!(payload["date_start"])
     date_end = Date.from_iso8601!(payload["date_end"])
@@ -287,16 +379,33 @@ defmodule WebposWeb.OrganizationChannel do
     list_day = list |> Enum.map(fn x -> x.day end) |> Enum.uniq()
 
     sales =
-      Repo.all(
-        from(
-          i in Reports.Sale,
-          where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
-          select: %{
-            salesdate: i.salesdate,
-            sales: i.grand_total
-          }
+      if payload["branch_name"] == "All Branch" do
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where:
+              i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+                i.organization_id == ^organization.id,
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
         )
-      )
+      else
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where:
+              i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+                i.organization_id == ^organization.id and i.rest_name == ^payload["branch_name"],
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
+        )
+      end
 
     final =
       for item <- list_day do
@@ -317,21 +426,13 @@ defmodule WebposWeb.OrganizationChannel do
 
         total
       end
-
-    IO.inspect(final)
 
     broadcast(socket, "today_sales_reply", %{result: final})
     {:noreply, socket}
   end
 
   def handle_in("this_week_sales", payload, socket) do
-    # broadcast(socket, "shout", payload)
-    IO.inspect(payload["date_start"])
-    IO.inspect(payload["date_end"])
-
-    IO.inspect(payload)
-    IO.inspect(socket)
-
+    organization = Repo.get_by(Organization, code: payload["organization_code"])
     date = Date.from_iso8601!(payload["date_start"])
 
     list = Date.range(Timex.beginning_of_week(date), Timex.end_of_week(date))
@@ -343,16 +444,31 @@ defmodule WebposWeb.OrganizationChannel do
     date_end = Timex.end_of_week(date)
 
     sales =
-      Repo.all(
-        from(
-          i in Reports.Sale,
-          where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
-          select: %{
-            salesdate: i.salesdate,
-            sales: i.grand_total
-          }
+      if payload["branch_name"] == "All Branch" do
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
         )
-      )
+      else
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where:
+              i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+                i.organization_id == ^organization.id and i.rest_name == ^payload["branch_name"],
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
+        )
+      end
 
     final =
       for item <- list_day do
@@ -374,22 +490,13 @@ defmodule WebposWeb.OrganizationChannel do
         total
       end
 
-    IO.inspect(final)
-
     broadcast(socket, "this_week_sales_reply", %{result: final})
     {:noreply, socket}
   end
 
   def handle_in("this_month_sales", payload, socket) do
-    # broadcast(socket, "shout", payload)
-    IO.inspect(payload["date_start"])
-    IO.inspect(payload["date_end"])
-
-    IO.inspect(payload)
-    IO.inspect(socket)
-
     date = Date.from_iso8601!(payload["date_start"])
-
+    organization = Repo.get_by(Organization, code: payload["organization_code"])
     list = Date.range(Timex.beginning_of_month(date), Timex.end_of_month(date))
 
     list_day = list |> Enum.map(fn x -> x.day end) |> Enum.uniq()
@@ -399,16 +506,31 @@ defmodule WebposWeb.OrganizationChannel do
     date_end = Timex.end_of_month(date)
 
     sales =
-      Repo.all(
-        from(
-          i in Reports.Sale,
-          where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
-          select: %{
-            salesdate: i.salesdate,
-            sales: i.grand_total
-          }
+      if payload["branch_name"] == "All Branch" do
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
         )
-      )
+      else
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where:
+              i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+                i.organization_id == ^organization.id and i.rest_name == ^payload["branch_name"],
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
+        )
+      end
 
     final =
       for item <- list_day do
@@ -430,20 +552,11 @@ defmodule WebposWeb.OrganizationChannel do
         total
       end
 
-    IO.inspect(final)
-
     broadcast(socket, "this_month_sales_reply", %{result: final})
     {:noreply, socket}
   end
 
   def handle_in("this_year_sales", payload, socket) do
-    # broadcast(socket, "shout", payload)
-    IO.inspect(payload["date_start"])
-    IO.inspect(payload["date_end"])
-
-    IO.inspect(payload)
-    IO.inspect(socket)
-
     date = Date.from_iso8601!(payload["date_start"])
 
     list = Date.range(Timex.beginning_of_year(date), Timex.end_of_year(date))
@@ -453,18 +566,36 @@ defmodule WebposWeb.OrganizationChannel do
     date_start = Timex.beginning_of_year(date)
 
     date_end = Timex.end_of_year(date)
+    organization = Repo.get_by(Organization, code: payload["organization_code"])
 
     sales =
-      Repo.all(
-        from(
-          i in Reports.Sale,
-          where: i.salesdate >= ^date_start and i.salesdate <= ^date_end,
-          select: %{
-            salesdate: i.salesdate,
-            sales: i.grand_total
-          }
+      if payload["branch_name"] == "All Branch" do
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where:
+              i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+                i.organization_id == ^organization.id,
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
         )
-      )
+      else
+        Repo.all(
+          from(
+            i in Reports.Sale,
+            where:
+              i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+                i.organization_id == ^organization.id and i.rest_name == ^payload["branch_name"],
+            select: %{
+              salesdate: i.salesdate,
+              sales: i.grand_total
+            }
+          )
+        )
+      end
 
     final =
       for item <- list_month do
@@ -486,16 +617,11 @@ defmodule WebposWeb.OrganizationChannel do
         total
       end
 
-    IO.inspect(final)
-
     broadcast(socket, "this_year_sales_reply", %{result: final})
     {:noreply, socket}
   end
 
   def handle_in("organization_branch", payload, socket) do
-    # broadcast(socket, "shout", payload)
-    IO.inspect(payload["organization_code"])
-
     organization = Repo.get_by(Organization, code: payload["organization_code"])
 
     code =
@@ -503,11 +629,11 @@ defmodule WebposWeb.OrganizationChannel do
         from(
           i in Webpos.Settings.Restaurant,
           where: i.organization_id == ^organization.id,
-          select: %{
-            name: i.name
-          }
+          select: i.name
         )
       )
+
+    IO.inspect(code)
 
     broadcast(socket, "organization_branch_reply", %{result: code})
     {:noreply, socket}
