259a8d557d481ed143c3a4738365e2d3022878ea
Your Nawi <asnawicsc@gmail.com>
2019-04-04 14:47:10 +0800

    update

--- lib/webpos_web/channels/organization_channel.ex
+++ lib/webpos_web/channels/organization_channel.ex
@@ -24,11 +24,9 @@ defmodule WebposWeb.OrganizationChannel do
 
   # Add authorization logic here as required.
   defp authorized?(payload, code) do
-    IO.inspect(payload)
     organization = Repo.get_by(Organization, code: code)
 
     password = Comeonin.Bcrypt.hashpwsalt(payload["license_key"])
-    IO.inspect(organization)
 
     organization =
       if organization != nil do
@@ -68,12 +66,7 @@ defmodule WebposWeb.OrganizationChannel do
         %{password: nil}
       end
 
-    IO.inspect(organization.password)
-
-    IO.inspect(payload["license_key"])
-
     a = payload["license_key"] == organization.password
-    IO.inspect(a)
 
     a
   end
@@ -209,6 +202,7 @@ defmodule WebposWeb.OrganizationChannel do
   end
 
   def handle_in("daily_sales", payload, socket) do
+    IO.inspect(payload)
     date_start = Date.from_iso8601!(payload["date_start"])
     date_end = Date.from_iso8601!(payload["date_end"])
 
@@ -254,6 +248,7 @@ defmodule WebposWeb.OrganizationChannel do
           else
             sales =
               sales
+              |> Enum.filter(fn x -> x.sales != nil end)
               |> Enum.map(fn x -> Decimal.to_float(x.sales) end)
               |> Enum.sum()
               |> Float.round(2)
@@ -319,6 +314,227 @@ defmodule WebposWeb.OrganizationChannel do
       end
 
     broadcast(socket, "daily_sales_reply", %{result: final, result2: result3})
+    {:noreply, socket}
+  end
+
+  def handle_in("daily_sales_all", payload, socket) do
+    IO.inspect(payload)
+    date_start = Date.from_iso8601!(payload["date_start"])
+    date_end = Date.from_iso8601!(payload["date_end"])
+
+    list = Date.range(date_start, date_end)
+
+    list_day = list |> Enum.map(fn x -> x.day end) |> Enum.uniq()
+    organization = Repo.get_by(Organization, code: payload["organization_code"])
+
+    code =
+      Repo.all(
+        from(
+          i in Webpos.Settings.Restaurant,
+          where: i.organization_id == ^organization.id,
+          select: i.name
+        )
+      )
+
+    IO.inspect(code)
+
+    all = ["All Branch"]
+
+    codes = all ++ code
+
+    all_branch =
+      Repo.all(
+        from(
+          i in Reports.Sale,
+          where:
+            i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+              i.organization_id == ^organization.id,
+          select: %{
+            salesdate: i.salesdate,
+            sales: i.grand_total,
+            rest_name: "All Branch"
+          }
+        )
+      )
+
+    sales =
+      Repo.all(
+        from(
+          i in Reports.Sale,
+          where:
+            i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+              i.organization_id == ^organization.id,
+          select: %{
+            salesdate: i.salesdate,
+            sales: i.grand_total,
+            rest_name: i.rest_name
+          }
+        )
+      )
+
+    sales = all_branch ++ sales
+
+    final =
+      for code <- codes do
+        sp_sales = sales |> Enum.filter(fn x -> x.rest_name == code end)
+
+        b =
+          for item <- list_day do
+            sales = sp_sales |> Enum.filter(fn x -> x.salesdate.day == item end)
+
+            total =
+              if sales == [] do
+                %{branch: code, day: item, sales: 0}
+              else
+                sales =
+                  sales
+                  |> Enum.filter(fn x -> x.sales != nil end)
+                  |> Enum.map(fn x -> Decimal.to_float(x.sales) end)
+                  |> Enum.sum()
+                  |> Float.round(2)
+
+                %{branch: code, day: item, sales: sales}
+              end
+
+            total
+          end
+
+        b
+      end
+      |> List.flatten()
+
+    al =
+      Repo.all(
+        from(i in Reports.Sale,
+          left_join: p in Reports.SalesDetail,
+          on: i.salesid == p.salesid,
+          where:
+            i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+              i.organization_id == ^organization.id,
+          group_by: [p.itemname],
+          order_by: [desc: sum(p.sub_total)],
+          select: %{
+            item: p.itemname,
+            sales: sum(p.sub_total),
+            rest_name: "All Branch"
+          },
+          limit: 10
+        )
+      )
+      |> Enum.filter(fn x -> x.item != nil end)
+
+    al2 =
+      Repo.all(
+        from(i in Reports.Sale,
+          left_join: p in Reports.SalesDetail,
+          on: i.salesid == p.salesid,
+          where:
+            i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+              i.organization_id == ^organization.id,
+          group_by: [p.itemname, i.rest_name],
+          order_by: [desc: sum(p.sub_total)],
+          select: %{
+            item: p.itemname,
+            sales: sum(p.sub_total),
+            rest_name: i.rest_name
+          }
+        )
+      )
+      |> Enum.filter(fn x -> x.item != nil end)
+
+    rec = al ++ al2
+
+    result3 =
+      for code <- codes do
+        rec_item = rec |> Enum.filter(fn x -> x.rest_name == code end)
+
+        for item <- rec_item do
+          sales = item.sales
+
+          new_sales =
+            if sales != 0.0 do
+              sales |> Decimal.to_string()
+            else
+              "0.0"
+            end
+
+          %{branch: code, item: item.item, sales: new_sales}
+        end
+      end
+      |> List.flatten()
+      |> Enum.filter(fn x -> x != [] end)
+
+    all_branch_sales_payment =
+      Repo.all(
+        from(
+          i in Reports.Sale,
+          left_join: k in Reports.SalesPayment,
+          on: i.salesid == k.salesid,
+          where:
+            i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+              i.organization_id == ^organization.id,
+          select: %{
+            payment_type: k.payment_type,
+            sales: i.grand_total,
+            rest_name: "All Branch"
+          }
+        )
+      )
+
+    sales_payment =
+      Repo.all(
+        from(
+          i in Reports.Sale,
+          where:
+            i.salesdate >= ^date_start and i.salesdate <= ^date_end and
+              i.organization_id == ^organization.id,
+          select: %{
+            payment_type: k.payment_type,
+            sales: i.grand_total,
+            rest_name: i.rest_name
+          }
+        )
+      )
+
+    payment_type = organization.payments |> String.split(",")
+
+    sales_p = all_branch_sales_payment ++ sales_payment
+
+    final_sales_payment =
+      for code <- codes do
+        spp_sales = sales_p |> Enum.filter(fn x -> x.rest_name == code end)
+
+        b =
+          for pt <- payment_type do
+            sales = spp_sales |> Enum.filter(fn x -> x.payment_type == pt end)
+
+            total =
+              if sales == [] do
+                %{branch: code, payment_type: pt, sales: 0}
+              else
+                sales =
+                  sales
+                  |> Enum.filter(fn x -> x.sales != nil end)
+                  |> Enum.map(fn x -> Decimal.to_float(x.sales) end)
+                  |> Enum.sum()
+                  |> Float.round(2)
+
+                %{branch: code, payment_type: pt, sales: sales}
+              end
+
+            total
+          end
+
+        b
+      end
+      |> List.flatten()
+
+    broadcast(socket, "daily_sales_reply_all", %{
+      result: final,
+      result2: result3,
+      final_sales_payment: final_sales_payment
+    })
+
     {:noreply, socket}
   end
 
@@ -634,6 +850,10 @@ defmodule WebposWeb.OrganizationChannel do
       )
 
     IO.inspect(code)
+
+    all = ["All Branch"]
+
+    code = all ++ code
 
     broadcast(socket, "organization_branch_reply", %{result: code})
     {:noreply, socket}
